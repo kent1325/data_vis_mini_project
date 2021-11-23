53de9834271dec170fd142c1664c7c63e64b515d
Kent Vugs Nielsen <19304326+kent1325@users.noreply.github.com>
2021-11-23 14:45:16 +0100

    added navbar and css

--- ui.R
+++ ui.R
@@ -1,7 +1,12 @@
 # Here we define the design and layout of our R Shiny application (Bootstrap).
 shinyUI(
   fluidPage(
-  # Here we let a submodule do the graph plot for us. (ggplot)
-  basic_ggplot_UI("basic_fast_plot")
+  # Define a navigation panel.
+  # This is the navigation bar in our application. Within each tabPanel
+  # assign a label to the menu item and then we call our submodules.
+  navlistPanel(id = "pageChooser", well= FALSE, widths=c(2,10),
+    tabPanel("Data Investigator", basic_ggplot_UI("basic_fast_plot")),
+    #tabPanel("Linked Plots", page_linked_plots_UI("linked_plots_example"))
+    ),
   )
 )
